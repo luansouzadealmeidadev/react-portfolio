26f1624df1bcbf6a163f7cc69bf5e81c1c9206b5
luansouzadealmeidadev <155761695+luansouzadealmeidadev@users.noreply.github.com>
2024-01-07 08:00:05 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Styles from './App.module.css'
+import { Navbar } from './components/Navbar/Navbar'
+
+function App() {
+ 
+
+  return (
+    <div className={Styles.App}>
+     <Navbar />
+    </div>
+  )
+}
+
+export default App
