6c6ee7ac18519648c704b62379fe98bbee81fedc
luan.almeida <luan.almeida@trizy.app>
2024-01-08 08:21:08 -0300

    feat:add navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,25 @@
 
-export const Navbar=()=>{
-    return(
-        <div>Navbar</div>
+export const Navbar = () => {
+    return (
+        <nav>
+            <a href="/">Portfolio</a>
+            <div>
+                <ul>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                    <li>
+                        <a href="#experience">Experience</a>
+                    </li>
+                    <li>
+                        <a href="#projects">Projects</a>
+                    </li>
+                    <li>
+                    <a href="#contact">Contact</a>                         
+                    </li>
+                </ul>
+            </div>
+        </nav>
 
     )
 }
