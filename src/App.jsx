776a8a27e225f31c501e47a675be61f552e9887e
luan.almeida <luan.almeida@trizy.app>
2024-01-08 10:28:17 -0300

    worked in src

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-
-import Styles from './App.module.css'
-import { Navbar } from './components/Navbar/Navbar'
+import styles from "./App.module.css";
+import { About } from "./components/About/About";
+import { Contact } from "./components/Contact/Contact";
+import { Experience } from "./components/Experience/Experience";
+import { Hero } from "./components/Hero/Hero";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Projects } from "./components/Projects/Projects";
 
 function App() {
- 
-
   return (
-    <div className={Styles.App}>
-     <Navbar />
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:luansouzadealmeidadev@gmail.com">luansouzadealmeidadev@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/luan-souza-de-almeida-531093169/">linkedin.com/luan-souza-de-almeida</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/luansouzadealmeidadev">github.com/luansouzadealmeidadev</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
